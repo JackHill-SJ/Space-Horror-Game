79cadfb77598754aeb0f548528d118d87fc9ec6c
Sean <43484827+DoctorOum@users.noreply.github.com>
2021-10-26 22:51:02 -0400

    Implemented input system so game now works keyboard and mouse or controller. Camera viewing needs a little work

--- Space Horror Game/Assets/Scripts/PlayerScripts/PCCameraController.cs
+++ Space Horror Game/Assets/Scripts/PlayerScripts/PCCameraController.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PCCameraController : MonoBehaviour
 {
+    [SerializeField] InputActionAsset playerControls;
+    InputAction LookAction;
+    Vector3 lookDirection;
+
     public float RotationSpeed = 1;
     public Transform Target, Player;
     float mouseX, mouseY;
+
+    private void Awake()
+    {
+        var PlayerActionMap = playerControls.FindActionMap("Player");
+        LookAction = PlayerActionMap.FindAction("Look");
+
+        LookAction.performed += LookInput;
+        LookAction.canceled += LookInput;
+    }
+    private void OnEnable()
+    {
+        LookAction.Enable();
+    }
+    private void OnDisable()
+    {
+        LookAction.Disable();
+    }
 
     void Start()
     {
@@ -16,13 +38,20 @@ public class PCCameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseX += lookDirection.x * (RotationSpeed * .5f);
+        mouseY -= lookDirection.z * (RotationSpeed * .5f);
         mouseY = Mathf.Clamp(mouseY, -35, 60);
 
         transform.LookAt(Target);
 
         Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
         Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+    public void LookInput(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+
+        lookDirection = new Vector3(direction.x, 0, direction.y);
     }
 }
--- Space Horror Game/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Space Horror Game/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class PlayerMovement : MonoBehaviour
     Vector3 Direction;
     Rigidbody rb;
 
+    [SerializeField] InputActionAsset PlayerControls;
+    InputAction movementAction, jumpAction, runAction;
+    Vector3 moveDirection;
+    bool isRunning;
+
     public int maxStamina = 100; //max amount of stamina a player has
     [SerializeField]
     int currentStamina; //current calculataed amount of stamina player has
@@ -20,19 +26,49 @@ public class PlayerMovement : MonoBehaviour
 
     public GroundCheck groundCheck;
 
-    private void Awake() => rb = gameObject.GetComponent<Rigidbody>();
+    private void Awake() 
+    {
+        var PlayerActionMap = PlayerControls.FindActionMap("Player");
+        movementAction = PlayerActionMap.FindAction("Move");
+        jumpAction = PlayerActionMap.FindAction("Jump");
+        runAction = PlayerActionMap.FindAction("Run");
+
+        movementAction.performed += InputMovement;
+        movementAction.canceled += InputMovement;
+        jumpAction.performed += Jumping;
+        jumpAction.canceled += Jumping;
+        runAction.performed += Running;
+        runAction.canceled += Running;
+
+
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+
+    private void OnEnable()
+    {
+        movementAction.Enable();
+        jumpAction.Enable();
+        runAction.Enable();
+    }
+    private void OnDisable()
+    {
+        movementAction.Disable();
+        jumpAction.Disable();
+        runAction.Disable();
+    }
 
     private void Start() => ResetStamina();
 
     void Update()
     {
         //Input
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        //horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
 
         Direction = Vector3.zero;
-        Direction = transform.forward * vertical;
-        Direction += transform.right * horizontal;
+        Direction = transform.forward * moveDirection.z;
+        Direction += transform.right * moveDirection.x;
 
         //just incase the current stamina goes over the max stamina it will reset itself back down to the set max amount of stamina
         if (currentStamina >= maxStamina)
@@ -43,18 +79,12 @@ public class PlayerMovement : MonoBehaviour
     {
         //Moving
         MoveCharacter();
-
-        if (ShouldJump)
-        {
-            rb.AddForce(Vector3.up * jumpForce);
-        }
     }
 
     public void MoveCharacter()
     { 
-        
         //running will make it player go faster. Set to be 1.5x faster than the speed of walking
-        if (/*Input.GetAxis("Run") > 0.1f*/ Input.GetKey(KeyCode.LeftShift) && !staminaDepleted)
+        if (/*Input.GetAxis("Run") > 0.1f Input.GetKey(KeyCode.LeftShift)*/ isRunning && !staminaDepleted)
         {
             Direction *= (speed * 1.5f) * Time.fixedDeltaTime;
             DepleteStamina(staminaRate);
@@ -67,14 +97,38 @@ public class PlayerMovement : MonoBehaviour
             {
                 StartCoroutine(IncreaseStamina(staminaRate));
             }
-
         }
 
         rb.MovePosition(transform.position + (Direction));
     }
 
     //Jump button set in unity input system is being pressed
-    private bool ShouldJump => Input.GetAxis("Jump") > 0.1f && groundCheck.isGrounded;
+    //private bool ShouldJump => jumpAction.triggered && groundCheck.isGrounded;
+
+    private void Running(InputAction.CallbackContext context)
+    {
+        if (context.ReadValueAsButton())
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+    }
+
+    public void Jumping(InputAction.CallbackContext context)
+    {
+        if (groundCheck.isGrounded)
+            rb.AddForce(Vector3.up * jumpForce);
+    }
+
+    public void InputMovement(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+
+        moveDirection = new Vector3(direction.x, 0, direction.y);
+    }
 
     private void DepleteStamina(int depletionAmount)
     {
